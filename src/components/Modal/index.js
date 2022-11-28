9ba2dc3aa5b62b3bcfdfe97adbc0f530f276b9a8
Charlie Carter <charliec1665@gmail.com>
2022-11-28 17:03:11 -0500

    created modal for large images

---
+++ src/components/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// destructuring the props in PhotoList into currentPhoto here in the parameter
+function Modal({currentPhoto}) {
+    // destructure currentPhoto properties into constants to assign values in the modal
+    const {name, category, description, index} = currentPhoto;
+
+    return (
+        // JSX
+        <div className='modalBackdrop'>
+            <div className='modalContainer'>
+                <h3 className='modalTitle'>{name}</h3>
+                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt='current category' />
+                <p>{description}</p>
+                <button type='button'>
+                    Close this modal
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import Modal from '../Modal'
 
 const PhotoList = ({category}) => {
+
+    // Hook to manage modal state
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const [photos] = useState([
         {
@@ -103,19 +107,30 @@ const PhotoList = ({category}) => {
     // going through each photo in photos array, every photo that matches the 'category' is returned in an array assigned currentPhotos
       const currentPhotos = photos.filter((photo) => photo.category === category);
 
+    // declare props for image and index data for modal to render
+    const [currentPhoto, setCurrentPhoto] = useState();
+
+    // function to use Hook to manage the state of the current photo
+    const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(true);
+    }
+
       return (
         <div>
-          <div className="flex-row">
-            {/* mapping currentPhotos array to render each photo that matches selected category */}
-            {currentPhotos.map((image, i) => (
-              <img
-                src={require(`../../assets/small/${category}/${i}.jpg`)}
-                alt={image.name}
-                className="img-thumbnail mx-1"
-                key={image.name}
-              />
-            ))}
-          </div>
+            {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+            <div className="flex-row">
+                {/* mapping currentPhotos array to render each photo that matches selected category */}
+                {currentPhotos.map((image, i) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${i}.jpg`)}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        onClick={() => toggleModal(image, i)}
+                        key={image.name}
+                    />
+                ))}
+            </div>
         </div>
       );
 }
